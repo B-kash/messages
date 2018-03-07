62e355f1fb8e2e82d5244d736220e03615c61409
Bikash Chapagain <bikash.chapagain@yco.com.np>
2018-03-07 15:55:00 +0545

    added middleware and JSONparser

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const loggingService = require('./app/services/loggingService');
 const  Suggestion = require('./app/models/Suggestion');
 console.log("Hello app started on 3000 port");
 let app = express();
-app.use(bodyParser.json());
+
+app.use( function (req, res,next) {
+	console.log("at my own middleware");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+    // res.end();
+});
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get("/", (req, res) => {
     res.send("Welcome");
 });
@@ -14,6 +26,9 @@ app.get('/messages',(req,res)=>{
 	res.send(service.getSuggestions());
 });
 app.post('/messages', (req, res) => {
+	console.log(req.body);
+	console.log("Entered into post method");
+	console.log(req.body);
 	let suggestion = new Suggestion(req.body);
 	res.send(service.postSuggestion(suggestion));
 });
